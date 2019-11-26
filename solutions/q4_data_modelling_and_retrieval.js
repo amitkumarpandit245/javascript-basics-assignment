df1d96d013af7461abb59ed3b38607bf13302467
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-26 16:33:13 +0000

    Modified

--- solutions/q4_data_modelling_and_retrieval.js
+++ solutions/q4_data_modelling_and_retrieval.js
@@ -1,7 +1,45 @@
-// Create a list of fruits with their properties (name, color, pricePerKg)
-// and convert it into a format so that for a given fruit name
-// retrieval of its color and pricePerKg value is fast
-
-
 // Write your code here
+// Create a list of fruits with their properties (name, color, pricePerKg)
+var fruits=
+[
+    {
+        name:"Apple",
+        color:"Red",
+        pricePerKg:"50"
+
+    },
+    {
+        name:"Banana",
+        color:"Yellow",
+        pricePerKg:"30"
+
+    },
+    {
+        name:"Mango",
+        color:"Pink",
+        pricePerKg:"20"
+
+    },
+    {
+        name:"Grapes",
+        color:"Green",
+        pricePerKg:"50"
+
+    }
+];
+var result={};
+//Function to Convert List Of Fruits in to Object format to retrieve value by Fruit name 
+const convertToObject=(fruits)=>{
+    fruits.forEach(element => {
+        result[element.name]={color:element.color,pricePerKg:element.pricePerKg};
+    });
+    //console.log(result);
+    return result;
+}
+///console.log(fruits);
+// and convert it into a format so that for a given fruit name
+var fastObject=convertToObject(fruits);
+// retrieval of its color and pricePerKg value is fast by providing fruit name
+console.log(fastObject["Mango"]);
+
 
--- solutions/q5_students_result_card.js
+++ solutions/q5_students_result_card.js
@@ -8,3 +8,83 @@
 
 
 // Write your code here
+var student=[
+    {
+        name:"Amit",
+        Grammer:67,
+        Accounts:78,
+        SubjectCategory:"GA"
+    },
+    {
+        name:"Santosh",
+        Grammer:69,
+        Accounts:87,
+        SubjectCategory:"GA"
+    },
+    {
+        name:"Satish",
+        Grammer:61,
+        Accounts:47,
+        SubjectCategory:"GA"
+    },
+    {
+        name:"Sachin",
+        Grammer:56,
+        Accounts:87,
+        SubjectCategory:"GA"
+    },
+    {
+        name:"Sehwag",
+        Grammer:69,
+        Accounts:87,
+        SubjectCategory:"GA",
+    },
+    {
+        name:"Bipul",
+        Grammer:62,
+        Accounts:89,
+        SubjectCategory:"GA"
+    },
+    {
+        name:"Gopal",
+        Grammer:54,
+        Physics:67,
+        SubjectCategory:"GP"
+    },
+    {
+        name:"Gopal",
+        Grammer:54,
+        Physics:67,
+        SubjectCategory:"GP"
+    },
+    {
+        name:"Amar",
+        Grammer:57,
+        Physics:87,
+        SubjectCategory:"GP"
+    },
+    {
+        name:"Aman",
+        Grammer:88,
+        Physics:97,
+        SubjectCategory:"GP"
+    },
+    {
+        name:"Rahul",
+        Grammer:98,
+        Physics:69,
+        SubjectCategory:"GP"
+    }
+];
+const getMarksSheet=(student)=>{
+    student.forEach(element => {
+        if(element.SubjectCategory==="GP"){
+            console.log("Name : "+element.name+"       Percentage : "+(element.Grammer+element.Physics)*0.5+" %");
+        }
+        else{
+            console.log("Name : "+element.name+"       Percentage : "+(element.Grammer+element.Accounts)*0.5+" %");
+        }
+        
+    });
+}
+getMarksSheet(student);
